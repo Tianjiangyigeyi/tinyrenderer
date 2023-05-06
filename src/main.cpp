89e9119517c832853931847946c89ba19530662a
BKK <787885589@qq.com>
2023-05-06 19:04:19 +0800

    zbuffer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#define INF 0x3f3f3f3f
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -11,8 +12,10 @@ Model *model = NULL;
 const int width = 800;
 const int height = 800;
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
+void triangle(Vec3f *screen_coords, float *zbuffer, TGAImage &image, TGAColor color);
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 bool InsideTriangle(Vec2i t0, Vec2i t1, Vec2i t2, int x, int y);
+Vec3f Barycentric(Vec3f *vertex, Vec2f p);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -79,23 +82,37 @@ int main(int argc, char **argv)
 
     TGAImage image(width, height, TGAImage::RGB);
     Vec3f light_dir(0, 0, -1);
+    float *zbuffer = new float[width * height];
+    for (int i = 0; i < width * height; ++i)
+    {
+        zbuffer[i] = -INF;
+    }
+    /////////////////////////////////////////////////////////////////////////////
     for (int i = 0; i < model->nfaces(); i++)
     {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
+        // Vec2i screen_coords[3];
+        Vec3f screen_coords[3];
         Vec3f world_coords[3];
         for (int j = 0; j < 3; j++)
         {
             Vec3f v0 = model->vert(face[j]);
             world_coords[j] = v0;
-            screen_coords[j] = Vec2i((v0.x + 1) * width / 2, (v0.y + 1) * height / 2);
+            // screen_coords[j] = Vec2i((v0.x + 1) * width / 2, (v0.y + 1) * height / 2);
+            // from [-1,1] to [0, width or height]
+            screen_coords[j] = Vec3f((v0.x + 1) * width / 2, (v0.y + 1) * height / 2, v0.z);
         }
+        // 计算的不是面的法线, 而是面的法线的反向向量, 因为要和入射光的方向点乘得到光照强度
         Vec3f n = ((world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0])).normalize();
         float intensity = n * light_dir;
+        intensity *= intensity;
         if (intensity > 0)
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        {
+            // triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+            triangle(screen_coords, zbuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        }
     }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
@@ -153,3 +170,50 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
     }
 }
 
+//with z-buffer
+void triangle(Vec3f *screen_coords, float *zbuffer, TGAImage &image, TGAColor color)
+{
+    float max[2], min[2];
+    min[0] = width;
+    min[1] = height;
+    max[0] = max[1] = 0;
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            max[j] = screen_coords[i].raw[j] < max[j] ? max[j] : screen_coords[i].raw[j];
+            min[j] = screen_coords[i].raw[j] < min[j] ? screen_coords[i].raw[j] : min[j];
+        }
+    }
+    for (int i = min[0]; i <= max[0] ; ++i)
+    {
+        for (int j = min[1]; j <= max[1] ; ++j)
+        {
+            Vec3f barycentric_coords = Barycentric(screen_coords, Vec2f(i, j));
+            if (barycentric_coords.x >= 0 && barycentric_coords.y >= 0 && barycentric_coords.z >= 0)
+            {
+                float z_new = screen_coords[0].z * barycentric_coords.x + screen_coords[1].z * barycentric_coords.y + screen_coords[2].z * barycentric_coords.z;
+                if (z_new > zbuffer[i * width + j])
+                {
+                    image.set(i, j, color);
+                    zbuffer[i * width + j] = z_new;
+                }
+            }
+        }
+    }
+}
+
+Vec3f Barycentric(Vec3f *vertex, Vec2f p)
+{
+    Vec3f res;
+    Vec2f v[3];
+    for (int i = 0; i < 3; ++i)
+    {
+        v[i].x = vertex[i].x;
+        v[i].y = vertex[i].y;
+    }
+    res.raw[0] = ((v[1] - p) ^ (v[2] - p)) / ((v[1] - v[0]) ^ (v[2] - v[0]));
+    res.raw[1] = ((v[2] - p) ^ (v[0] - p)) / ((v[2] - v[1]) ^ (v[0] - v[1]));
+    res.raw[2] = 1 - res.x - res.y;
+    return res;
+}
